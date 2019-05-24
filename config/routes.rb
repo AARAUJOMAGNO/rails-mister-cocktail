f1ed62af7e89cc90cbc4dfc00593e6a74cdc98d3
Adriano Araujo <aaraujo2000@gmail.com>
2019-05-24 12:32:22 -0300

    Create root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'doses/description'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'cocktails#index'
+  root to: 'cocktails#index'
   resources :doses, only: %i[destroy]
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
